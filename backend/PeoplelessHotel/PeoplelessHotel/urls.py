a6939fedd1e676edd775c389155a7b11078a0504
Andreas Andreou <Andreas.Andreou@ncr.com>
2020-07-28 15:19:55 +0300

    Install drf-yasg

--- backend/PeoplelessHotel/PeoplelessHotel/urls.py
+++ backend/PeoplelessHotel/PeoplelessHotel/urls.py
@@ -19,6 +19,25 @@ from django.conf.urls import url, include
 from django.views.generic import TemplateView, RedirectView
 from django.views.static import serve
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('guests/', include("guests.urls")),
@@ -27,4 +46,7 @@ urlpatterns = [
     path('reservations/', include("reservations.urls")),
     path('reservations_rooms/', include("reservations_rooms.urls")),
     path('rooms/', include("rooms.urls")),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
