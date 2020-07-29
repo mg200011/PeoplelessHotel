1857084b2318ae0602ba031ca9a5f5630d36f0cf
Andreas Andreou <Andreas.Andreou@ncr.com>
2020-07-29 16:56:20 +0300

    Backend - Authorization

--- backend/PeoplelessHotel/PeoplelessHotel/settings.py
+++ backend/PeoplelessHotel/PeoplelessHotel/settings.py
@@ -93,6 +93,14 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
