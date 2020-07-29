74c2775a16867dd70a6ac297ff502e54b6813d2a
Mariano J Gonzalez Sanchez <marianojgs@gmail.com>
2020-07-29 11:19:36 -0300

    ui changes

--- backend/PeoplelessHotel/PeoplelessHotel/static/assets/js/services.js
+++ backend/PeoplelessHotel/PeoplelessHotel/static/assets/js/services.js
@@ -80,14 +80,10 @@ class Services {
 
             const response = await fetch(url, {
                 method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
                 cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
                 headers: {
                   'Content-Type': 'application/json; charset=UTF-8'
                 },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
                 body: JSON.stringify(data) // body data type must match "Content-Type" header
             });
       
--- backend/PeoplelessHotel/PeoplelessHotel/static/assets/js/servicesproxy.js
+++ backend/PeoplelessHotel/PeoplelessHotel/static/assets/js/servicesproxy.js
@@ -4,8 +4,23 @@ class ServicesProxy {
 
     async getReservations() {
         const s = new Services();
-        return await s.gget("/reservations");
+        let list = await s.gget("/reservations/get_user_reservations/");
+        return list;
     }
+
+    async getReservation(resrv) {
+        const s = new Services();
+        let list = await s.gget(`/reservations/get_user_reservation_by_id/${resrv}/`);
+        return list[0];
+    }
+
+    
+    async updateReservation(resrv) {
+        const s = new Services();
+        let list = await s.gput(`/reservations/${resrv.id}`, resrv);
+        return list;
+    }
+
 
     async getRooms() {
         const s = new Services();
