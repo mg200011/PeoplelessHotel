46d58be5aadba4afb9b3d5df85cc81641e3de708
Andreas Andreou <Andreas.Andreou@ncr.com>
2020-07-30 00:13:14 +0300

    Update URLs

--- backend/PeoplelessHotel/guests/urls.py
+++ backend/PeoplelessHotel/guests/urls.py
@@ -14,7 +14,7 @@ router.register(r'', GuestsService, basename="guests")
 urlpatterns = router.urls
 
 urlpatterns += [url(r'^create_person_group/(?P<reservation_id>[0-9]+)/$', GuestsService.create_person_group),]
-urlpatterns += [url(r'^add_person_to_group/(?P<person_group_id>[0-9]+)/(?P<guest_id>[0-9]+)/$', GuestsService.add_person_to_group),]
-urlpatterns += [url(r'^train_person_group/(?P<person_group_id>[0-9]+)/$', GuestsService.train_person_group),]
+urlpatterns += [url(r'^add_person_to_group/(?P<person_group_id>[-\w+\d+]+)/(?P<guest_id>[0-9]+)/$', GuestsService.add_person_to_group),]
+urlpatterns += [url(r'^train_person_group/(?P<person_group_id>[-\w+\d+]+)/$', GuestsService.train_person_group),]
 urlpatterns += [url(r'^identify_in_person_group/(?P<room_id>[0-9]+)/$', GuestsService.identify_in_person_group),]
-urlpatterns += [url(r'^delete_person_group/(?P<person_group_id>[0-9]+)/$', GuestsService.delete_person_group),]
+urlpatterns += [url(r'^delete_person_group/(?P<person_group_id>[-\w+\d+]+)/$', GuestsService.delete_person_group),]
